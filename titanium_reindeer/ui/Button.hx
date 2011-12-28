e8f418ad211324f19c40000202bc52ff2f0a70a1
Ryan Badour <rybadour@gmail.com>
2011-12-27 21:01:10 -0500

    Added: A simple RectButton class, derived from a common button class. This is the meager beginnings of a UI library written for the engine. The RectButton contains a set of properties and methods to develop a simple rectangular button and the base class provides 3 states, idle, mouseOver, and heldDown.

---
+++ titanium_reindeer/ui/Button.hx
@@ -0,0 +1,189 @@
+package titanium_reindeer.ui;
+
+import titanium_reindeer.TextRenderer;
+import titanium_reindeer.CollisionComponent;
+import titanium_reindeer.MouseRegionHandler;
+import titanium_reindeer.CollisionComponentManager;
+import titanium_reindeer.Enums;
+
+class Button extends GameObject
+{
+	public static inline var UI_COLLISION_LAYER:String 	= "__primary_ui_collision_layer__";
+	public static inline var BUTTON_COLLISION_GROUP:String 	= "__primary_button_collision_group__";
+
+
+	public var shownText(default, null):TextRenderer;
+	public var text(default, setText):String;
+	public function setText(value:String):String
+	{
+		if (value != this.text)
+		{
+			this.text = value;
+
+			if (this.shownText != null)
+				this.shownText.text = this.text;
+		}
+
+		return this.text;
+	}
+
+	private var collisionShape:CollisionComponent;
+	private var mouseHandler:MouseRegionHandler;
+
+	private var registeredMouseClickEvents:Array<Vector2 -> Void>;
+
+	public var isMouseOver(default, null):Bool;
+	public var isHeldDown(default, null):Bool;
+
+	public var enabled(default, setEnabled):Bool;
+	private function setEnabled(value:Bool):Bool
+	{
+		if (value != this.enabled)
+		{
+			this.enabled = value;
+
+			if (!this.enabled)
+			{
+				this.disable();
+			}
+		}
+
+		return this.enabled;
+	}
+	
+
+	public function new(fgLayer:Int, collision:CollisionComponent)
+	{
+		super();
+
+		this.text = "";
+		this.shownText = new TextRenderer(this.text, fgLayer);
+		this.shownText.fillColor = Color.Black;
+		this.addComponent("__shown_text__", this.shownText);
+
+		this.collisionShape = collision;
+		this.addComponent("__collision_shape__", this.collisionShape);
+
+		this.registeredMouseClickEvents = new Array();
+
+		this.isMouseOver = false;
+		this.isHeldDown = false;
+
+		this.enabled = true;
+	}
+
+	// Publicly usable methods
+	public function registerMouseClickEvent(func:Vector2 -> Void):Void
+	{
+		if (func == null)
+			return;
+			
+		this.registeredMouseClickEvents.push(func);	
+	}
+
+	// Internal methods
+	private override function hasInitialized():Void
+	{
+		super.hasInitialized();
+
+		var collisionManager:CollisionComponentManager = cast(this.objectManager.getManager(CollisionComponentManager), CollisionComponentManager);
+		this.mouseHandler = collisionManager.mouseRegionManager.getHandler(this.collisionShape);
+		this.mouseHandler.registerMouseMoveEvent(MouseRegionMoveEvent.Enter, mouseEnter);
+		this.mouseHandler.registerMouseMoveEvent(MouseRegionMoveEvent.Exit, mouseExit);
+		this.mouseHandler.registerMouseButtonEvent(MouseRegionButtonEvent.Down, mouseDown);
+		this.mouseHandler.registerMouseButtonEvent(MouseRegionButtonEvent.Up, mouseUp);
+
+		this.objectManager.game.inputManager.registerMouseButtonEvent(MouseButton.Left, MouseButtonState.Up, mouseUpGlobal);
+	}
+
+	private function enable():Void
+	{
+	}
+
+	private function disable():Void
+	{
+		if (this.isMouseOver)
+		{
+			this.isMouseOver = false;
+			this.mouseOverStop();
+		}
+
+		if (this.isHeldDown)
+		{
+			this.isHeldDown = false;
+			this.heldDownStop();
+		}
+	}
+
+	private function mouseEnter(mousePos:Vector2):Void
+	{
+		if (!this.isMouseOver && this.enabled)
+		{
+			this.isMouseOver = true;
+			this.mouseOverStart();
+		}
+	}
+
+	private function mouseExit(mousePos:Vector2):Void
+	{
+		if (this.isMouseOver && this.enabled)
+		{
+			this.isMouseOver = false;
+			this.mouseOverStop();
+		}
+	}
+
+	private function mouseDown(mousePos:Vector2, button:MouseButton):Void
+	{
+		if (button == MouseButton.Left && !this.isHeldDown && this.enabled)
+		{
+			this.isHeldDown = true;
+			this.heldDownStart();
+		}
+	}
+
+	private function mouseUp(mousePos:Vector2, button:MouseButton):Void
+	{
+		if (button == MouseButton.Left && this.isHeldDown && this.enabled)
+		{
+			this.isHeldDown = false;
+			this.heldDownStop();
+			this.click(mousePos);
+		}
+	}
+	private function mouseUpGlobal(mousePos:Vector2):Void
+	{
+		if (this.isHeldDown && this.enabled)
+		{
+			this.isHeldDown = false;
+			this.heldDownStop();
+		}
+	}
+
+	private function mouseOverStart():Void
+	{
+	}
+
+	private function mouseOverStop():Void
+	{
+	}
+
+	private function heldDownStart():Void
+	{
+	}
+
+	private function heldDownStop():Void
+	{
+	}
+
+	private function click(mousePos:Vector2):Void
+	{
+		if (!this.enabled)
+			return;
+
+		for (func in this.registeredMouseClickEvents)
+		{
+			func(mousePos);
+		}
+	}
+}
---
+++ titanium_reindeer/ui/RectButton.hx
@@ -0,0 +1,102 @@
+package titanium_reindeer.ui;
+
+import titanium_reindeer.RectRenderer;
+import titanium_reindeer.CollisionRect;
+import titanium_reindeer.Color;
+
+class RectButton extends Button
+{
+	public var shownRect(default, null):RectRenderer;
+	private var collisionRect(default, null):CollisionRect;
+
+	public var width(default, setWidth):Float;
+	private function setWidth(value:Float):Float
+	{
+		if (value != this.width)
+		{
+			this.width = value;
+			this.shownRect.width = this.width;
+			this.collisionRect.width = this.width;
+		}
+
+		return this.width;
+	}
+
+	public var height(default, setHeight):Float;
+	private function setHeight(value:Float):Float
+	{
+		if (value != this.height)
+		{
+			this.height = value;
+			this.shownRect.height = this.height;
+			this.collisionRect.height = this.height;
+		}
+
+		return this.height;
+	}
+
+	public function new(width:Float, height:Float, bgLayer:Int, fgLayer:Int)
+	{
+		this.collisionRect = new CollisionRect(width, height, Button.UI_COLLISION_LAYER, Button.BUTTON_COLLISION_GROUP);
+
+		super(fgLayer, collisionRect);
+
+		this.shownRect = new RectRenderer(width, height, bgLayer);
+		this.shownRect.fillColor = Color.White;
+		this.shownRect.strokeColor = Color.Black;
+		this.shownRect.lineWidth = 1;
+		this.addComponent("__shown_rect__", shownRect);
+	}
+
+	private override function enable():Void
+	{
+		super.enable();
+	}
+
+	private override function disable():Void
+	{
+		super.disable();
+
+		var disableColor:Color = this.shownRect.fillColor;
+		disableColor.red = Math.round(disableColor.red * 0.4);
+		disableColor.green = Math.round(disableColor.green * 0.4);
+		disableColor.blue = Math.round(disableColor.blue * 0.4);
+		this.shownRect.fillColor = disableColor;
+	}
+
+	private override function mouseOverStart():Void
+	{
+		super.mouseOverStart();
+
+		this.shownRect.lineWidth += 1;
+	}
+
+	private override function mouseOverStop():Void
+	{
+		super.mouseOverStop();
+
+		this.shownRect.lineWidth -= 1;
+	}
+
+	private override function heldDownStart():Void
+	{
+		super.heldDownStart();
+
+		var idleColor:Color = this.shownRect.fillColor;
+		idleColor.red = Math.round(idleColor.red * 0.8);
+		idleColor.green = Math.round(idleColor.green * 0.8);
+		idleColor.blue = Math.round(idleColor.blue * 0.8);
+		this.shownRect.fillColor = idleColor;
+	}
+
+	private override function heldDownStop():Void
+	{
+		super.heldDownStop();
+
+		var heldColor:Color = this.shownRect.fillColor;
+		heldColor.red = Math.round(heldColor.red * 1.25);
+		heldColor.green = Math.round(heldColor.green * 1.25);
+		heldColor.blue = Math.round(heldColor.blue * 1.25);
+		this.shownRect.fillColor = heldColor;
+	}
+}
