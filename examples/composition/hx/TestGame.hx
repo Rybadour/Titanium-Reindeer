5636db8ad5c73cd20b0325fb8c308286e07add09
Ryan Badour <rybadour@gmail.com>
2012-03-18 01:42:52 -0400

    Updated: The composition example project now works with the new scene code.
    Signed-off-by: Ryan Badour <rybadour@gmail.com>

--- examples/composition/hx/TestGame.hx
+++ examples/composition/hx/TestGame.hx
@@ -1,26 +1,8 @@
 import titanium_reindeer.Game;
-import titanium_reindeer.GameObject;
-import titanium_reindeer.MovementComponent;
-import titanium_reindeer.RectRenderer;
-import titanium_reindeer.CircleRenderer;
-import titanium_reindeer.Vector2;
-import titanium_reindeer.Color;
-import titanium_reindeer.RendererComponent;
-import titanium_reindeer.InputManager;
-import titanium_reindeer.ImageRenderer;
-import titanium_reindeer.ImageSource;
-import titanium_reindeer.Rect;
-import titanium_reindeer.Circle;
-import titanium_reindeer.UnsaturateEffect;
-import titanium_reindeer.PixelatedEffect;
-import titanium_reindeer.TextRenderer;
-import titanium_reindeer.StrokeFillRenderer;
-import titanium_reindeer.Pattern;
-import titanium_reindeer.ColorStop;
-import titanium_reindeer.LinearGradient;
-import titanium_reindeer.Shadow;
+import titanium_reindeer.Scene;
 import titanium_reindeer.CollisionComponentManager;
-import titanium_reindeer.Enums;
+import titanium_reindeer.Color;
+import titanium_reindeer.Vector2;
 
 class TestGame extends Game
 {
@@ -31,11 +13,14 @@ class TestGame extends Game
 	}
 
     
+	public var globalScene:Scene;
     public var thing:Thing;
 
 	public function new()
 	{
-		super("TestGame", 800, 500, Layers.NUM_LAYERS, true, Color.Black);
+		super("TestGame", 800, 500, true);
+
+		this.globalScene = new Scene(this, "globalScene", 0, Layers.NUM_LAYERS, Color.Black);
 
         this.thing = new Thing(this.globalScene);
 		this.thing.position = new Vector2(200, 200);
