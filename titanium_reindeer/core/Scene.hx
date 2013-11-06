51111980ebb4e2de5061f4c6261941b7d14c4ccd
Ryan Badour <rybadour@gmail.com>
2013-11-06 13:34:05 -0500

    Added basic scene class

--- titanium_reindeer/core/Scene.hx
+++ titanium_reindeer/core/Scene.hx
@@ -1,66 +1,21 @@
 package titanium_reindeer.core;
 
 class Scene 
-	implements IGroup<IHasUpdater>,
-	implements IHasIdProvider,
-	implements IHasUpdater,
-	implements IHasId
 {
-	public var idProvider(default, null):IdProvider;
-	public var updater(default, null):Updater;
+	public var isPaused(default, null):Bool;
 
-	public var id(default, null):Int;
-	public var name(default, null):String;
-
-	private var hasProvider:IHasIdProvider;
-	private var objects:IntHash<IHasUpdater>;
-
-	public function new(provider:IHasIdProvider, name:String)
+	public function new()
 	{
-		this.hasProvider = provider;
-		this.id = this.hasProvider.idProvider.requestId();
-
-		this.name = name;
-		this.idProvider = new IdProvider();
-		this.updater = new Updater(preUpdate, update, postUpdate);
-
-		this.objects = new IntHash();
+		this.isPaused = false;
 	}
 
-	// As a Group
-	public function get(id:Int):IHasUpdater
+	public function pause():Void
 	{
-		if ( !this.objects.exists(id) )
-			return null;
-
-		return this.objects.get(id);
-	}
-	public function add(id:Int, object:IHasUpdater):Void
-	{
-		this.objects.set(id, object);
-	}
-	public function remove(id:Int):Void
-	{
-		if ( !this.objects.exists(id) )
-			return;
-
-		this.objects.remove(id);
+		this.isPaused = true;
 	}
 
-	// Real-time updates
-	private function preUpdate(msTimeStep:Int):Void
+	public function unpause():Void
 	{
-		for (obj in objects)
-			obj.updater.preUpdate(msTimeStep);
-	}
-	private function update(msTimeStep:Int):Void
-	{
-		for (obj in objects)
-			obj.updater.update(msTimeStep);
-	}
-	private function postUpdate(msTimeStep:Int):Void
-	{
-		for (obj in objects)
-			obj.updater.postUpdate(msTimeStep);
+		this.pause = false;
 	}
 }
