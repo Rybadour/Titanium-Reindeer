80a386866607edbf0516bc7a31c871bff2590fe6
Ryan Badour <rybadour@gmail.com>
2013-12-12 15:10:55 -0500

    Commented core/Scene.hx to completion

--- titanium_reindeer/core/Scene.hx
+++ titanium_reindeer/core/Scene.hx
@@ -1,7 +1,13 @@
 package titanium_reindeer.core;
 
+/**
+ * The Scene class is the base class for something which needs to be updated and paused. Each call 
+ * to update the scene is a step in time. A game instance is usually responsible for propogating the
+ * initial time step update.
+ */
 class Scene 
 {
+	// Flag for the paused state of the Scene
 	public var isPaused(default, null):Bool;
 
 	public function new()
@@ -9,9 +15,12 @@ class Scene
 		this.isPaused = false;
 	}
 
+	/**
+	 * A call to update causes the frame to simulate one time step
+	 */
 	public function update(msTimeStep:Int):Void
 	{
-		if ( ! this.isPaused)
+		if ( !this.isPaused )
 		{
 			this.preUpdate(msTimeStep);
 			this._update(msTimeStep);
@@ -19,26 +28,29 @@ class Scene
 		}
 	}
 
+	/**
+	 * Set the scene to the paused state.
+	 * This prevents time steps from taking place.
+	 */
 	public function pause():Void
 	{
 		this.isPaused = true;
 	}
 
+	/**
+	 * Set the scene to the unpaused state.
+	 * This allows time steps to take place.
+	 */
 	public function unpause():Void
 	{
 		this.isPaused = false;
 	}
 
-	// Real-time updates
-	private function preUpdate(msTimeStep:Int):Void
-	{
-	}
-
-	private function _update(msTimeStep:Int):Void
-	{
-	}
-
-	private function postUpdate(msTimeStep:Int):Void
-	{
-	}
+	/**
+	 * These functions are designed to be overridden by the sub-class.
+	 * They are called automatically when update() is called, but only if the scene is unpaused.
+	 */
+	private function preUpdate(msTimeStep:Int):Void {}
+	private function _update(msTimeStep:Int):Void {}
+	private function postUpdate(msTimeStep:Int):Void {}
 }
