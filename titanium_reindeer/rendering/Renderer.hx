0cbf0effaea7cf672c0698ce8f023bd0f6c2a077
Ryan Badour <rybadour@gmail.com>
2013-12-13 09:58:25 -0500

    Removed position from rendering/Renderer.hx
     - It has a bad code smell and a quick check revealed it hasn't been used yet.

--- titanium_reindeer/rendering/Renderer.hx
+++ titanium_reindeer/rendering/Renderer.hx
@@ -3,18 +3,15 @@ package titanium_reindeer.rendering;
 class Renderer<S:IRenderState> implements IRenderer
 {
 	public var state:S;
-	public var position:Vector2;
 
 	public function new(state:S)
 	{
-		this.position = new Vector2(0, 0);
 		this.state = state;
 	}
 
 	public function render(canvas:Canvas2D):Void
 	{
 		canvas.ctx.save();
-		canvas.ctx.translate(this.position.x, this.position.y);
 		this.state.apply(canvas);
 		this._render(canvas);
 		canvas.ctx.restore();
