883b7c30914795a533edcbfc050b3006544dd769
Ryan Badour <rybadour@gmail.com>
2013-12-13 13:41:52 -0500

    Commented rendering/TextState.hx to completion

--- titanium_reindeer/rendering/TextState.hx
+++ titanium_reindeer/rendering/TextState.hx
@@ -1,5 +1,8 @@
 package titanium_reindeer.rendering;
 
+/**
+ * The following enums allow typed configuration of some font properties of text.
+ */
 enum FontStyle
 {
 	Normal; Italics; Oblique;
@@ -15,6 +18,11 @@ enum FontWeight
 	Normal; Bold; Size(s:Int);
 }
 
+/**
+ * A TextState object encompasses the configuration options for rendering text.
+ * A TextState is also a StrokeFillState so the strokes and fill properties of the text can be
+ * configured through the properties exposed by the StrokeFillState class.
+ */
 class TextState extends StrokeFillState
 {
 	public var fontStyle:FontStyle;
@@ -27,35 +35,40 @@ class TextState extends StrokeFillState
 	{
 		super();
 
-		this.fontStyle = FontStyle.Normal;
+		// Same as the defaults of the html5 specification
+		this.fontStyle   = FontStyle.Normal;
 		this.fontVariant = FontVariant.Normal;
-		this.fontWeight = FontWeight.Normal;
-		this.fontSize = 10;
-		this.fontFamily = "sans-serif";
+		this.fontWeight  = FontWeight.Normal;
+		this.fontSize    = 10;
+		this.fontFamily  = "sans-serif";
 	}
 
+	/**
+	 * This function applies the current state of this object to a given canvas element.
+	 */
 	public override function apply(canvas:Canvas2D)
 	{
 		super.apply(canvas);
 
+		// Get the string representation of each property
 		var fontParts:Array<String> = new Array();
 		switch (fontStyle)
 		{
-			case Normal: fontParts.push("normal");
+			case Normal:  fontParts.push("normal");
 			case Italics: fontParts.push("italics");
 			case Oblique: fontParts.push("oblique");
 		}
 
 		switch (fontVariant)
 		{
-			case Normal: fontParts.push("normal");
+			case Normal:    fontParts.push("normal");
 			case SmallCaps: fontParts.push("small-caps");
 		}
 
 		switch (fontWeight)
 		{
-			case Normal: fontParts.push("normal");
-			case Bold: fontParts.push("bold");
+			case Normal:  fontParts.push("normal");
+			case Bold:    fontParts.push("bold");
 			case Size(s): fontParts.push(""+Math.max(100, Math.min(s, 900)));
 		}
 
@@ -66,6 +79,9 @@ class TextState extends StrokeFillState
 		// TODO: Either use this and apply an offset in UIText (and any where else)
 		//       Or don't center the text by default
 		//canvas.ctx.textAlign = "center";
-		canvas.ctx.textBaseline = "middle";
+		//
+		// TODO: These properties should be managed publicly through this class
+		//canvas.ctx.textBaseline = "middle";
+		
 	}
 }
