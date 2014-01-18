cd8e67cc686e2d5dd4999a24845f984e6c70cb20
Ryan Badour <rybadour@gmail.com>
2014-01-18 17:30:42 -0500

    Commented core/IUnique.hx

--- titanium_reindeer/core/IUnique.hx
+++ titanium_reindeer/core/IUnique.hx
@@ -1,6 +1,12 @@
 package titanium_reindeer.core;
 
+/**
+ * Represents a object who can be identified uniquely but the getKey() function.
+ */
 interface IUnique<K>
 {
+	/**
+	 * Intended to return a unique key for objects of implementing this interface.
+	 */
 	public function getKey():K;
 }
