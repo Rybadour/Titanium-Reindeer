3f7bbc730d5a40238bfe24c5ff80005b0d57ef91
Ryan Badour <rybadour@gmail.com>
2014-04-08 13:32:11 -0400

    Updated broken TmxXmlTests
     - Still needs to be completed though

--- unit_testing/tiles/TmxXmlTests.hx
+++ unit_testing/tiles/TmxXmlTests.hx
@@ -13,7 +13,6 @@ class TmxXmlTests extends haxe.unit.TestCase
 	public static inline var TMX_CSV_TEST_FILE:String = 'tiles/csv_test.tmx';
 	public static inline var TMX_XML_TEST_FILE:String = 'tiles/xml_test.tmx';
 
-	public var tmxMap:TmxMap;
 	public var tmxXmlWithCsv:TmxXml;
 	public var tmxXmlWithXml:TmxXml;
 	public var tmxCsvData:String;
@@ -29,25 +28,25 @@ class TmxXmlTests extends haxe.unit.TestCase
 	{
 		this.tmxXmlWithCsv = new TmxXml(Xml.parse(this.tmxCsvData));
 
-		assertEquals(this.tmxXml.version, "1.0");
-		assertEquals(this.tmxXml.orientation, TileMapOrientation.Orthogonal);
-		assertEquals(this.tmxXml.width, 30);
-		assertEquals(this.tmxXml.height, 30);
-		assertEquals(this.tmxXml.tileWidth, 16);
-		assertEquals(this.tmxXml.tileHeight, 16);
-		assertTrue( Color.Black.equal(this.tmxXml.backgroundColor) );
+		assertEquals(this.tmxXmlWithCsv.version, "1.0");
+		assertEquals(this.tmxXmlWithCsv.orientation, TileMapOrientation.Orthogonal);
+		assertEquals(this.tmxXmlWithCsv.width, 30);
+		assertEquals(this.tmxXmlWithCsv.height, 30);
+		assertEquals(this.tmxXmlWithCsv.tileWidth, 16);
+		assertEquals(this.tmxXmlWithCsv.tileHeight, 16);
+		assertTrue( Color.Black.equal(this.tmxXmlWithCsv.backgroundColor) );
 	}
 
 	public function testParseXmlWithXmlData()
 	{
 		this.tmxXmlWithXml = new TmxXml(Xml.parse(this.tmxXmlData));
 
-		assertEquals(this.tmxXml.version, "1.0");
-		assertEquals(this.tmxXml.orientation, TileMapOrientation.Orthogonal);
-		assertEquals(this.tmxXml.width, 30);
-		assertEquals(this.tmxXml.height, 30);
-		assertEquals(this.tmxXml.tileWidth, 16);
-		assertEquals(this.tmxXml.tileHeight, 16);
-		assertTrue( Color.Black.equal(this.tmxXml.backgroundColor) );
+		assertEquals(this.tmxXmlWithXml.version, "1.0");
+		assertEquals(this.tmxXmlWithXml.orientation, TileMapOrientation.Orthogonal);
+		assertEquals(this.tmxXmlWithXml.width, 30);
+		assertEquals(this.tmxXmlWithXml.height, 30);
+		assertEquals(this.tmxXmlWithXml.tileWidth, 16);
+		assertEquals(this.tmxXmlWithXml.tileHeight, 16);
+		assertTrue( Color.Black.equal(this.tmxXmlWithXml.backgroundColor) );
 	}
 }
