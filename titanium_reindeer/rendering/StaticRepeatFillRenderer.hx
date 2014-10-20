b1a54fe3fa14308667a89ba379d240c160a551a1
Ryan Badour <rybadour@gmail.com>
2014-10-19 20:56:18 -0400

    Finally finished implementing the new StaticRepeatFillRenderer

--- titanium_reindeer/rendering/StaticRepeatFillRenderer.hx
+++ titanium_reindeer/rendering/StaticRepeatFillRenderer.hx
@@ -1,8 +1,11 @@
 package titanium_reindeer.rendering;
 
+import titanium_reindeer.rendering.RepeatFillRenderer;
+
 class StaticRepeatFillRenderer extends RepeatFillRenderer
 {
 	public var renderer:IRenderer;
+	public var cached:Canvas2D;
 
 	public function new(renderer:IRenderer, width:Int, height:Int, method:RepeatFillMethod, ?sWidth:Int, ?sHeight:Int)
 	{
@@ -11,10 +14,12 @@ class StaticRepeatFillRenderer extends RepeatFillRenderer
 		this.renderer = renderer;
 	}
 
-	private override function preRender(canvas:Canvas2D):Void
+	private override function _render(canvas:Canvas2D):Void
 	{
 		this.cached = new Canvas2D('static_cache', this.sourceWidth, this.sourceHeight);
 		this.renderer.render(this.cached);
+
+		super._render(canvas);
 	}
 
 	private override function renderTile(x:Int, y:Int, canvas:Canvas2D):Void
