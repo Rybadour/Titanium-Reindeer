973d8dbe676c702900997b6e5d0d7551be980efe
Badour <gameboy@amazon.com>
2016-11-26 22:50:04 -0500

    Fixed ImageTileAtlas

--- titanium_reindeer/rendering/ImageTileAtlas.hx
+++ titanium_reindeer/rendering/ImageTileAtlas.hx
@@ -4,13 +4,12 @@ import js.html.Image;
 
 class ImageTileAtlas extends UniformTextureAtlas
 {
-	public var image:Image;
-
 	public function new(image:Image, tileWidth:Int, tileHeight:Int)
 	{
 		this.image = image;
 
-		super(tileWidth, tileHeight, image.width, image.height);
+		super(tileWidth, tileHeight);
+		this.createCanvas(this.image.width, this.image.height);
 		this.imagesCanvas.renderImage(this.image);
 	}
 }
