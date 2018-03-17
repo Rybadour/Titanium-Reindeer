d3ccf563aa61671925ffb070c3cfd36014b5ffd6
Badour <gameboy@amazon.com>
2018-03-17 19:20:10 -0400

    UniformTextureAtlas can handle source images with a margin between frames

--- titanium_reindeer/rendering/UniformTextureAtlas.hx
+++ titanium_reindeer/rendering/UniformTextureAtlas.hx
@@ -13,11 +13,26 @@ class UniformTextureAtlas implements ITextureAtlas
 
 	public var sourceTileWidth:Int;
 	public var sourceTileHeight:Int;
+	public var offsetX:Int;
+	public var offsetY:Int;
+	public var tileMarginX:Int;
+	public var tileMarginY:Int;
+	public var widthInTiles:Int;
 
-	public function new(sourceTileWidth:Int, sourceTileHeight:Int)
+	public function new(
+			sourceTileWidth:Int, sourceTileHeight:Int,
+			offsetX:Int = 0, offsetY:Int = 0,
+			tileMarginX:Int = 0, tileMarginY:Int = 0,
+			widthInTiles:Int = null
+	)
 	{
 		this.sourceTileWidth = sourceTileWidth;
 		this.sourceTileHeight = sourceTileHeight;
+		this.offsetX = offsetX;
+		this.offsetY = offsetY;
+		this.tileMarginX = tileMarginX;
+		this.tileMarginY = tileMarginY;
+		this.widthInTiles = widthInTiles;
 	}
 
 	public function createCanvas(width:Int, height:Int)
@@ -33,12 +48,24 @@ class UniformTextureAtlas implements ITextureAtlas
 		if (this.image == null && this.imagesCanvas == null)
 		  return;
 
-		var imageWidth = (this.image == null ? this.imagesCanvas.width : this.image.width);
+		var imageWidth = (this.image == null ? this.imagesCanvas.width : this.image.width) - offsetX;
 
-		var widthInTiles = Math.floor(imageWidth / this.sourceTileWidth);
+		var widthInTiles = this.widthInTiles;
+		if (widthInTiles == null)
+		{
+			var widthLeft = imageWidth;
+			var tiles = 0;
+			while (widthLeft > 0) {
+				widthLeft -= this.sourceTileWidth;
+				if (widthLeft <= 0)
+					break;
+				tiles++;
+				widthLeft -= this.tileMarginX;
+			}
+		}
 		// Render the part of the tile sheet that tile Id corresponds to
-		var sx = (index % widthInTiles) * this.sourceTileWidth;
-		var sy = Math.floor(index / widthInTiles) * this.sourceTileHeight;
+		var sx = (index % widthInTiles) * (this.sourceTileWidth + this.tileMarginX) + offsetX;
+		var sy = Math.floor(index / widthInTiles) * (this.sourceTileHeight + this.tileMarginY) + offsetY;
 		
 		canvas.ctx.drawImage(
 		  untyped __js__('(this.imagesCanvas == null ? this.image : this.imagesCanvas.canvas)'),
